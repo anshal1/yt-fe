e8521bebe02a58b328f2ca152ccd2be25d9e1e3f
amshal1 <panshal911@gmail.com>
2024-05-10 10:22:04 +0530

    showing duration

--- src/Components/VideoCard.jsx
+++ src/Components/VideoCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "../Styles/VideoCard.module.css";
 import { FormatDate, FormatNumber } from "../utils/FormatDate";
 import { useNavigate } from "react-router-dom";
@@ -6,6 +6,11 @@ import { truncateString } from "../utils/helperfunction";
 
 const VideoCard = ({ video }) => {
   const navigate = useNavigate();
+  const videoRef = useRef();
+  const [Duration, setDuration] = useState(0); // mins
+  useEffect(() => {
+    setDuration(Number(videoRef?.current?.duration / 60).toFixed(2));
+  }, [videoRef]);
   return (
     <div
       className={style["vid-card"]}
@@ -17,7 +22,9 @@ const VideoCard = ({ video }) => {
         src={video?.video?.["640x360"]}
         className={style["video-thumbnail"]}
         width={400}
+        ref={videoRef}
       ></video>
+      <span className={style["duration"]}>{Duration} min</span>
       <div className={style["details-container"]}>
         <div className={style["vid-details"]}>
           <p className={style["vid-title"]}>{truncateString(video?.title)}</p>
