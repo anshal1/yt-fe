fe32a677b5d5864e8e5b39665293465f277dbc8b
amshal1 <panshal911@gmail.com>
2024-05-16 13:45:35 +0530

    upload page UI created

--- src/Pages/Upload.jsx
+++ src/Pages/Upload.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Button from "../Components/Button";
 import { CatchErr } from "../utils/CatchErr";
 import { BASEURL } from "../utils/Constant";
+import style from "../Styles/Upload.module.css";
+import Input from "../Components/Input";
+import { FaUpload } from "react-icons/fa";
 
 const Upload = () => {
-  const [Video, setVideo] = useState(null);
+  const mediaRef = useRef();
+  const [VideoData, setVideoData] = useState({
+    video: null,
+    title: "",
+  });
+  const [Preview, setPreview] = useState("");
   const showProgress = (e) => {
     console.log(e.total);
     console.log(e.loaded);
@@ -34,17 +42,72 @@ const Upload = () => {
     });
   });
   const handleUpload = async () => {
-    const data = await UploadVideo({ title: "New Title", video: Video });
+    const data = await UploadVideo(VideoData);
     console.log(data);
   };
+
+  const handlePreview = CatchErr((file) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", (e) => {
+      setPreview(e.target.result);
+    });
+    reader.addEventListener("error", () => {
+      throw new Error("Error In Loading Video");
+    });
+    reader.readAsDataURL(file);
+  });
+
   return (
-    <div>
-      <input
-        type="file"
-        onChange={(e) => {
-          setVideo(e.target.files[0]);
-        }}
-      />
+    <div className={style["main"]}>
+      <div className={style["upload-section"]}>
+        <input
+          type="file"
+          style={{ display: "none" }}
+          ref={mediaRef}
+          onChange={(e) => {
+            handlePreview(e.target.files[0]);
+            setVideoData((prev) => {
+              return {
+                ...prev,
+                video: e.target.files[0],
+              };
+            });
+          }}
+        />
+      </div>
+      <div className={style["preview"]}>
+        {Preview ? (
+          <video src={Preview} controls width={"100%"} height={"100%"}></video>
+        ) : (
+          <div>
+            <FaUpload
+              style={{
+                cursor: "pointer",
+                display: "block",
+                width: "44px",
+                height: "44px",
+              }}
+              onClick={() => {
+                mediaRef?.current?.click();
+              }}
+            />
+          </div>
+        )}
+      </div>
+      <div className={style["title"]}>
+        <Input
+          placeholder={"Video Title"}
+          value={VideoData?.title}
+          onChange={(e) => {
+            setVideoData((prev) => {
+              return {
+                ...prev,
+                title: e.target.value,
+              };
+            });
+          }}
+        />
+      </div>
       <Button onClick={handleUpload}>Upload</Button>
     </div>
   );
